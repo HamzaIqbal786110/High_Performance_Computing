93a60da2f18c865bb5fe22f8f9d221702759aa70
Hamza Iqbal <hamza.n.iqbal@gmail.com>
2025-04-08 03:49:17 -0400

    Removed uneccessary stuff

--- HW5/q1/omp/run_omp_histogram.sh
+++ HW5/q1/omp/run_omp_histogram.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
+OUTPUT_FILE="omp_histogram_results.txt"
+echo "OMP Histogram Benchmarking" > $OUTPUT_FILE
+echo "===========================" >> $OUTPUT_FILE
+echo "" >> $OUTPUT_FILE
+
 for ((exp=12; exp<=23; exp++)); do
     N=$((2 ** exp))
-    echo "Running for N = $N"
-    ./omp_histogram $N > omp_histogram_$N.out
+    echo "Running for N = $N" | tee -a $OUTPUT_FILE
+    ./omp_histogram $N >> $OUTPUT_FILE 2>&1
+    echo "--------------------------------------" >> $OUTPUT_FILE
+    echo "" >> $OUTPUT_FILE
 done
